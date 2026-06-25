4ba27b0fb6819c94df35e3d777bc2473f4b1cdf2
dhruvinka <dhruvink66@gmail.com>
2026-06-25 18:25:45 +0530

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.py
@@ -0,0 +1,39 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack=[]
+        self.Minval=None
+        
+
+    def push(self, val: int) -> None:
+        if self.Minval is None:
+            self.Minval=val
+        else:
+            self.Minval=min(self.Minval,val)
+
+
+
+        self.stack.append((self.Minval,val))
+
+        
+
+    def pop(self) -> None:
+        if len(self.stack) == 0:
+            return
+        else:
+            self.stack.pop()
+        
+
+    def top(self) -> int:
+        if len(self.stack) == 0:
+            return
+        else:
+            return self.stack[len(self.stack)-1][1]
+        
+
+    def getMin(self) -> int:
+         if len(self.stack) == 0:
+            return
+         else:
+            return self.stack[len(self.stack)-1][0]
+        
