b93447fdc9ef0362299a8b1579c9d18bae28d183
dhruvinka <dhruvink66@gmail.com>
2026-06-26 18:59:58 +0530

    Add: number-of-one-bits - submission-2

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        c=0
+        n=str(n)
+        for i in n:
+            if i == 1:
+                c+=1
+        return c
+        
